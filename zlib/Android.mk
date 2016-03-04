d3e32b08e9d0ac27280af62ea6d2057db36cf99e
osm0sis <osm0sis@outlook.com>
2016-03-04 17:53:10 -0400

    zlib: fix NDK builds, make static library only

--- zlib/Android.mk
+++ zlib/Android.mk
@@ -22,24 +22,6 @@ zlib_files := \
 	src/uncompr.c \
 	src/zutil.c
 
-LOCAL_MODULE := libz
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
-
-# TODO: This is to work around b/24465209. Remove after root cause is fixed
-LOCAL_LDFLAGS_arm := -Wl,--hash-style=both
-
-LOCAL_SRC_FILES := $(zlib_files)
-ifneq ($(TARGET_BUILD_APPS),)
-  LOCAL_SDK_VERSION := 9
-else
-  LOCAL_CXX_STL := none
-endif
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
 LOCAL_ARM_MODE := arm
 LOCAL_MODULE := libz
 LOCAL_MODULE_TAGS := optional
@@ -54,50 +36,3 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-
-LOCAL_MODULE := libz
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
-LOCAL_SRC_FILES := $(zlib_files)
-LOCAL_MULTILIB := both
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
-LOCAL_MODULE_HOST_OS := darwin linux windows
-LOCAL_CXX_STL := none
-include $(BUILD_HOST_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := libz-host
-LOCAL_MODULE_TAGS := optional
-LOCAL_CFLAGS += -O3 -DUSE_MMAP
-LOCAL_SRC_FILES := $(zlib_files)
-LOCAL_MULTILIB := both
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
-LOCAL_CXX_STL := none
-include $(BUILD_HOST_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:=        \
-	src/test/minigzip.c
-
-LOCAL_MODULE:= gzip
-
-LOCAL_SHARED_LIBRARIES := libz
-
-LOCAL_CXX_STL := none
-
-include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:=        \
-	src/test/minigzip.c
-
-LOCAL_MODULE:= minigzip
-
-LOCAL_STATIC_LIBRARIES := libz
-
-LOCAL_CXX_STL := none
-
-include $(BUILD_HOST_EXECUTABLE)
