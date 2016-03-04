d2f34e6b2f502abec0100d3cd60921ad297b68a9
osm0sis <osm0sis@outlook.com>
2016-03-04 18:31:01 -0400

    build+libandroidfw: hack around CursorWindow compile - CursorWindow appears to only compile with c++ STL but several other components of libandroidfw only compile with gnu STL so split it into its own Makefile, and compile it first with c++_static STL so its object can then be used by the subsequent gnu STL compile of libandroidfw

--- build.sh
+++ build.sh
@@ -14,6 +14,10 @@ fi;
 
 for i in libandroidfw libcutils liblog libutils zlib zopfli; do
   cd $i;
+  if [ "$i" == libandroidfw ]; then
+    # CursorWindow appears to require a different STL; compile it first so its object can be used later
+    $NDK_ROOT/ndk-build NDK_PROJECT_PATH=. NDK_OUT=../obj NDK_LIBS_OUT=../libs APP_BUILD_SCRIPT=./CursorWindowHack.mk NDK_TOOLCHAIN=arm-linux-androideabi-4.9 APP_PLATFORM=android-21 APP_STL=c++_static;
+  fi;
   $NDK_ROOT/ndk-build NDK_PROJECT_PATH=. NDK_OUT=../obj NDK_LIBS_OUT=../libs APP_BUILD_SCRIPT=./Android.mk NDK_TOOLCHAIN=arm-linux-androideabi-4.9 APP_PLATFORM=android-21 APP_STL=gnustl_static;
   cd ..;
 done;
