e4e9c92ea47f415dd51adbb6fd89314236d3d25b
osm0sis <osm0sis@outlook.com>
2016-03-04 22:54:22 -0400

    build: allow changing toolchain+platform parameters - APP_PLATFORM and NDK_TOOLCHAIN may be set beforehand to build for different architectures

--- build.sh
+++ build.sh
@@ -10,18 +10,23 @@ esac;
 if [ ! "$NDK_ROOT" ]; then
   NDK_ROOT=~/android/android-ndk-r*;
 fi;
-
+if [ ! "$NDK_TOOLCHAIN" ]; then
+  NDK_TOOLCHAIN=arm-linux-androideabi-4.9;
+fi;
+if [ ! "$APP_PLATFORM" ]; then
+  APP_PLATFORM=android-21;
+fi;
 
 for i in libandroidfw libcutils liblog libutils zlib zopfli; do
   cd $i;
   if [ "$i" == libandroidfw ]; then
     # CursorWindow appears to require a different STL; compile it first so its object can be used later
-    $NDK_ROOT/ndk-build NDK_PROJECT_PATH=. NDK_OUT=../obj NDK_LIBS_OUT=../libs APP_BUILD_SCRIPT=./CursorWindowHack.mk NDK_TOOLCHAIN=arm-linux-androideabi-4.9 APP_PLATFORM=android-21 APP_STL=c++_static;
+    $NDK_ROOT/ndk-build NDK_PROJECT_PATH=. NDK_OUT=../obj NDK_LIBS_OUT=../libs APP_BUILD_SCRIPT=./CursorWindowHack.mk NDK_TOOLCHAIN=$NDK_TOOLCHAIN APP_PLATFORM=$APP_PLATFORM APP_STL=c++_static;
   fi;
-  $NDK_ROOT/ndk-build NDK_PROJECT_PATH=. NDK_OUT=../obj NDK_LIBS_OUT=../libs APP_BUILD_SCRIPT=./Android.mk NDK_TOOLCHAIN=arm-linux-androideabi-4.9 APP_PLATFORM=android-21 APP_STL=gnustl_static;
+  $NDK_ROOT/ndk-build NDK_PROJECT_PATH=. NDK_OUT=../obj NDK_LIBS_OUT=../libs APP_BUILD_SCRIPT=./Android.mk NDK_TOOLCHAIN=$NDK_TOOLCHAIN APP_PLATFORM=$APP_PLATFORM APP_STL=gnustl_static;
   cd ..;
 done;
 
-$NDK_ROOT/ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_TOOLCHAIN=arm-linux-androideabi-4.9 APP_PLATFORM=android-21 APP_STL=gnustl_static;
+$NDK_ROOT/ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_TOOLCHAIN=$NDK_TOOLCHAIN APP_PLATFORM=$APP_PLATFORM APP_STL=gnustl_static;
 exit 0;
 
