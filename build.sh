4cdf0c7b3cbe2629b0288072d63126f5d3777a02
osm0sis <osm0sis@outlook.com>
2017-02-08 12:44:21 -0400

    build: fix for newest NDK and Cygwin

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+case "$(uname -s)" in
+  CYGWIN*|MINGW32*|MSYS*) ext=.cmd;
+esac;
+
 case $1 in
   clean)
     rm -rf libs obj;
@@ -13,6 +17,9 @@ fi;
 if [ ! "$NDK_TOOLCHAIN" ]; then
   NDK_TOOLCHAIN=arm-linux-androideabi-4.9;
 fi;
+if [ ! "$APP_ABI" ]; then
+  APP_ABI=armeabi;
+fi;
 if [ ! "$APP_PLATFORM" ]; then
   APP_PLATFORM=android-21;
 fi;
@@ -21,12 +28,12 @@ for i in libandroidfw libcutils liblog libutils zlib zopfli; do
   cd $i;
   if [ "$i" == libandroidfw ]; then
     # CursorWindow appears to require a different STL; compile it first so its object can be used later
-    $NDK_ROOT/ndk-build NDK_PROJECT_PATH=. NDK_OUT=../obj NDK_LIBS_OUT=../libs APP_BUILD_SCRIPT=./CursorWindowHack.mk NDK_TOOLCHAIN=$NDK_TOOLCHAIN APP_PLATFORM=$APP_PLATFORM APP_STL=c++_static;
+    $NDK_ROOT/ndk-build$ext NDK_PROJECT_PATH=. NDK_OUT=../obj NDK_LIBS_OUT=../libs APP_BUILD_SCRIPT=./CursorWindowHack.mk NDK_TOOLCHAIN=$NDK_TOOLCHAIN APP_ABI=$APP_ABI APP_PLATFORM=$APP_PLATFORM APP_STL=c++_static;
   fi;
-  $NDK_ROOT/ndk-build NDK_PROJECT_PATH=. NDK_OUT=../obj NDK_LIBS_OUT=../libs APP_BUILD_SCRIPT=./Android.mk NDK_TOOLCHAIN=$NDK_TOOLCHAIN APP_PLATFORM=$APP_PLATFORM APP_STL=gnustl_static;
+  $NDK_ROOT/ndk-build$ext NDK_PROJECT_PATH=. NDK_OUT=../obj NDK_LIBS_OUT=../libs APP_BUILD_SCRIPT=./Android.mk NDK_TOOLCHAIN=$NDK_TOOLCHAIN APP_ABI=$APP_ABI APP_PLATFORM=$APP_PLATFORM APP_STL=gnustl_static;
   cd ..;
 done;
 
-$NDK_ROOT/ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_TOOLCHAIN=$NDK_TOOLCHAIN APP_PLATFORM=$APP_PLATFORM APP_STL=gnustl_static;
+$NDK_ROOT/ndk-build$ext NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk NDK_TOOLCHAIN=$NDK_TOOLCHAIN APP_ABI=$APP_ABI APP_PLATFORM=$APP_PLATFORM APP_STL=gnustl_static;
 exit 0;
 
